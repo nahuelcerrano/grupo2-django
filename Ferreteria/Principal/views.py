f805659ffdd15bebfdec4004261c21edda77b974
nahuelcerrano <nahuelcerrano21@gmail.com>
2023-04-17 21:35:10 -0300

    Hotfix

--- Ferreteria/Principal/views.py
+++ Ferreteria/Principal/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
- 
+from .forms import Formulario_Contacto
 
 def home(request):
     return render(request, 'principal/home.html')
 
 def contacto(request):
-    return render(request, 'principal/contacto.html')
 
+    form = Formulario_Contacto()
+    if request.method == 'POST':
+        nombreContacto = request.POST.get('nombreContacto')
+        emailContacto = request.POST.get('emailContacto')
+        telefonoContacto = request.POST.get('telefonoContacto')
+        mensajeContacto = request.POST.get('mensajeContacto')
+        (nombreContacto, emailContacto, telefonoContacto, mensajeContacto)
+    
+    context = {
+        'formulario': form
+    }
+
+    # NO HACE NADA POR AHORA, NO ESTA TERMINADO
+
+    return render(request, 'principal/contacto.html', context)
+
+def lineas(request):
+    pass
